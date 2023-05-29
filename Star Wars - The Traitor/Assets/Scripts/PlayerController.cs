bfafc6ca2e37e2a5807db0f92a3eaf70aa02cd69
Mykyta Tishkin <87891550+mykytatishkin@users.noreply.github.com>
2023-05-29 16:47:18 +0300

    animation and menu update

--- Star Wars - The Traitor/Assets/Scripts/PlayerController.cs
+++ Star Wars - The Traitor/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,7 @@ using static System.TimeZoneInfo;
 
 public class PlayerController : MonoBehaviour
 {
-    public float stopTime = 1f; // Время, за которое персонаж остановится (в секундах)
-    public float moveSpeed = 12f; // Скорость движения персонажа
+    private float moveSpeed = 12f; // Скорость движения персонажа
     public Animator animator; // Ссылка на компонент Animator
 
     private float currentSpeed; // Текущая скорость движения персонажа
@@ -48,9 +47,48 @@ public class PlayerController : MonoBehaviour
             isMoving = false;
             animator.SetBool("IsMoving", false);
             animator.CrossFade("Rifle_Idle", 0.15f);
-
         }
 
-       
+        // Получаем ввод с мыши
+        float mouseX = Input.GetAxis("Mouse X");
+
+        // Поворачиваем персонаж по оси Y в соответствии с вращением мыши
+        transform.Rotate(Vector3.up, mouseX);
+
+        MovingAnimationSetter("MovingForward", "Rifle_Walk",KeyCode.W, 12f);
+        MovingAnimationSetter("MovingBack", "Rifle_Walk_Back", KeyCode.S, 12f);
+        MovingAnimationSetter("MovingRight", "Rifle_Walk_Right", KeyCode.D, 3f);
+        MovingAnimationSetter("MovingLeft", "Rifle_Walk_Left", KeyCode.A, 3f);
+
+
+        if (Input.GetKey(KeyCode.T))
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+        }
+        if (Input.GetKey(KeyCode.Y))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
     }
+
+    private void MovingAnimationSetter(string animationFlag,string animationName, KeyCode keyCode, float moveSpeedNew)
+    {
+        if (Input.GetKeyDown(keyCode))
+        {
+
+            animator.SetBool(animationFlag, true);
+            animator.CrossFade(animationName, 0.15f);
+            moveSpeed = moveSpeedNew;
+        }
+        else if (Input.GetKeyUp(keyCode))
+        {
+            animator.SetBool(animationFlag, false);
+            moveSpeed = 12f;
+        }
+
+    }
+
 }
